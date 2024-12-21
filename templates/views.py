18a16339113032480094a402ebbda9056a92e44b
shubhamojha98 <shubhamojha.ojha098@gmail.com>
2024-12-21 14:12:15 +0530

    initial commit

---
+++ templates/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+def  index (request):
+    return render(request,'index.html')
+
+def home (request):
+    return render (request,'home.html')
+
+def about (request):
+    return render (request,'about.html')
+
